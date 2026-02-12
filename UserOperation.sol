1e8e189a146dcdf0c866fd2078be00c2a222e198
Sujon <160887695+sujon12210@users.noreply.github.com>
2026-02-12 13:30:08 +0600

    Create UserOperation.sol

---
+++ UserOperation.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @dev Minimal UserOperation struct definition as per ERC-4337.
+ */
+struct UserOperation {
+    address sender;
+    uint256 nonce;
+    bytes initCode;
+    bytes callData;
+    uint256 callGasLimit;
+    uint256 verificationGasLimit;
+    uint256 preVerificationGas;
+    uint256 maxFeePerGas;
+    uint256 maxPriorityFeePerGas;
+    bytes paymasterAndData;
+    bytes signature;
+}
